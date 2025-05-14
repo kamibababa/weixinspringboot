407eadbd6ff60f05de0cbdb4befa66ccb95216a6
lisi <lisi@qq.com>
2025-05-14 10:41:03 +0800

    modify

--- src/main/java/com/tencent/wxcloudrun/controller/CounterController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/CounterController.java
@@ -109,30 +109,30 @@ public class CounterController {
    * @param request {@link CounterRequest}
    * @return API response json
    */
-  @PostMapping(value = "/api/count")
-  ApiResponse create(@RequestBody CounterRequest request) {
-    logger.info("/api/count post request, action: {}", request.getAction());
-
-    Optional<Counter> curCounter = counterService.getCounter(1);
-    if (request.getAction().equals("inc")) {
-      Integer count = 1;
-      if (curCounter.isPresent()) {
-        count += curCounter.get().getCount();
-      }
-      Counter counter = new Counter();
-      counter.setId(1);
-      counter.setCount(count);
-      counterService.upsertCount(counter);
-      return ApiResponse.ok(count);
-    } else if (request.getAction().equals("clear")) {
-      if (!curCounter.isPresent()) {
-        return ApiResponse.ok(0);
-      }
-      counterService.clearCount(1);
-      return ApiResponse.ok(0);
-    } else {
-      return ApiResponse.error("参数action错误");
-    }
-  }
-  
+//  @PostMapping(value = "/api/count")
+//  ApiResponse create(@RequestBody CounterRequest request) {
+//    logger.info("/api/count post request, action: {}", request.getAction());
+//
+//    Optional<Counter> curCounter = counterService.getCounter(1);
+//    if (request.getAction().equals("inc")) {
+//      Integer count = 1;
+//      if (curCounter.isPresent()) {
+//        count += curCounter.get().getCount();
+//      }
+//      Counter counter = new Counter();
+//      counter.setId(1);
+//      counter.setCount(count);
+//      counterService.upsertCount(counter);
+//      return ApiResponse.ok(count);
+//    } else if (request.getAction().equals("clear")) {
+//      if (!curCounter.isPresent()) {
+//        return ApiResponse.ok(0);
+//      }
+//      counterService.clearCount(1);
+//      return ApiResponse.ok(0);
+//    } else {
+//      return ApiResponse.error("参数action错误");
+//    }
+//  }
+//
 }
